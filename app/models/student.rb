172ad52abad0630aeb5876e21dc688920da0fd70
Kishandth Sivapalasundaram <rodes88@msn.com>
2019-06-24 11:50:28 +0000

    Automatically backed up by Learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -3,13 +3,5 @@ class Student < ActiveRecord::Base
   def to_s
     self.first_name + " " + self.last_name
   end
-  
-  def active(a=false)
-    if a == false 
-      "This student is currently inactive."
-    else 
-      "This student is currently active."
-    end  
-  end 
 
 end
