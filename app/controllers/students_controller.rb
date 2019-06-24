8cb43f5ea0aaf854828cbc8fabd78a6cc1080241
Kishandth Sivapalasundaram <rodes88@msn.com>
2019-06-24 11:58:00 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -13,7 +13,7 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
     @student.active = !@student.active
     @student.save
-    redirect to '/student/:id'
+    redirect to "/student/#{@student.id}"
   end 
 
   private
